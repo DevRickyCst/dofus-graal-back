018527502ea873813aadc28df7c962957f724271
DevRickyCst <dev.creusot.aym@gamil.com>
2025-02-09 17:15:10 +0100

    fix test ci

--- src/db/operations/crud.rs
+++ src/db/operations/crud.rs
@@ -22,6 +22,10 @@ where
 #[cfg(test)]
 mod insert_and_retrieve_record_test {
     use super::*;
+    use crate::models::statics::item::{
+        insertable::NewItem,
+        queryable::Item,
+    };
     use crate::models::statics::item_meta::insertable::*;
     use crate::models::statics::item_meta::queryable::{
         Effect, ImageUrls, ItemType, Range, Recipe,
@@ -29,14 +33,14 @@ mod insert_and_retrieve_record_test {
     use crate::scripts::delete_items::*;
     use crate::test_utils::{get_test_db_connection, setup_test_environment};
 
+
+
     #[test]
     fn item_type() {
         use crate::schema::item_types::dsl::*;
 
         setup_test_environment();
         let mut conn = get_test_db_connection();
-
-        let _ = delete_items(&mut conn);
 
         let new_record = NewItemType {
             id: 5,
@@ -55,7 +59,6 @@ mod insert_and_retrieve_record_test {
 
         setup_test_environment();
         let mut conn = get_test_db_connection();
-        let _ = delete_items(&mut conn);
 
         let new_image_urls = NewImageUrls {
             icon: "testing".to_string(),
@@ -79,7 +82,6 @@ mod insert_and_retrieve_record_test {
 
         setup_test_environment();
         let mut conn = get_test_db_connection();
-        let _ = delete_items(&mut conn);
 
         let new_range = NewRange { min: 0, max: 5 };
         let record: Range = insert_and_retrieve_record(new_range, ranges, &mut conn)
@@ -89,16 +91,60 @@ mod insert_and_retrieve_record_test {
         assert_eq!(record.max, 5);
     }
 
+    #[test] 
+    fn item(){
+        use crate::schema::items::dsl::*;
+
+        setup_test_environment();
+        let mut conn = get_test_db_connection();
+
+        let new_item = NewItem {
+            ankama_id: 1,
+            name: "Test Item",
+            description: "Test Description",
+            level: 1,
+            pods: Some(10),
+            image_urls_id: 1,
+            ap_cost: Some(3),
+            max_cast_per_turn: Some(2),
+            is_weapon: true,
+            is_two_handed: Some(false),
+            range_id: Some(1),
+            critical_hit_probability: Some(5),
+            critical_hit_bonus: Some(10),
+            type_id: 1,
+            category_id: 1,
+        };
+
+        let record: Item = insert_and_retrieve_record(new_item, items, &mut conn)
+            .expect("Erreur lors de l'insertion des items");
+
+        assert_eq!(record.ankama_id, 1);
+        assert_eq!(record.name, "Test Item");
+        assert_eq!(record.description, "Test Description");
+        assert_eq!(record.level, 1);
+        assert_eq!(record.pods, Some(10));
+        assert_eq!(record.image_urls_id, 1);
+        assert_eq!(record.ap_cost, Some(3));
+        assert_eq!(record.max_cast_per_turn, Some(2));
+        assert_eq!(record.is_weapon, true);
+        assert_eq!(record.is_two_handed, Some(false));
+        assert_eq!(record.range_id, Some(1));
+        assert_eq!(record.critical_hit_probability, Some(5));
+        assert_eq!(record.critical_hit_bonus, Some(10));
+        assert_eq!(record.type_id, 1);
+        assert_eq!(record.category_id, 1);
+    }
+    
     #[test]
     fn test_recipe() {
         use crate::schema::recipes::dsl::*;
 
         setup_test_environment();
         let mut conn = get_test_db_connection();
-        let _ = delete_items(&mut conn);
 
         let new_recipe = NewRecipe {
-            item_id: None,
+            item_id: Some(1),
             item_ankama_id: 1,
             item_subtype: "Ingredient".to_string(),
             quantity: 10,
@@ -118,10 +164,9 @@ mod insert_and_retrieve_record_test {
 
         setup_test_environment();
         let mut conn = get_test_db_connection();
-        let _ = delete_items(&mut conn);
 
         let new_effect = NewEffect {
-            item_id: None,
+            item_id: Some(1),
             int_minimum: 5,
             int_maximum: 10,
             element_id: Some(1),
@@ -140,4 +185,7 @@ mod insert_and_retrieve_record_test {
         assert_eq!(record.ignore_int_max, true);
         assert_eq!(record.formatted, "Test Effect");
     }
+
+
+
 }
